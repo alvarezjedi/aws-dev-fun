84051303de6e571919a31941106e3c4e1aa45b62
alvarezjedi <alvarezjedi@gmail.com>
2017-10-18 21:46:23 -0400

    py_main Detected faces from local file system and added in/out files
    Signed-off-by: alvarezjedi <alvarezjedi@gmail.com>

--- py_main.py
+++ py_main.py
@@ -1,21 +1,48 @@
 #!/usr/bin/python -tt
 
-import sys
+import boto3
+import pprint
 
-# Define a main() function that prints a little greeting.
+#for compare_faces call
+SIMILARITY_THRESHOLD = 85
+
 
 def main():
-# Get the name from the command line, using 'World' as a fallback.
-    if len(sys.argv) >= 2:
-        name = sys.argv[1]
-    else:
-        name = input("Wait, what's your name? > ")
+    if __name__ == '__main__':
+        #credentials are handled by ~/.aws/credentials file
+        client = boto3.client('rekognition' )
+        
+        #test print 
+        #print(SIMILARITY_THRESHOLD)
+        
+        # Open source image in the project folder
+        with open('source.jpg', 'rb') as source_image:
+            source_bytes = source_image.read()
     
-    print ('Hello', name)
+        # Open target image in the project folder
+        with open('target.jpg', 'rb') as target_image:
+            target_bytes = target_image.read()
+        
+        response = client.compare_faces(
+                       SourceImage={ 'Bytes': source_bytes },
+                       TargetImage={ 'Bytes': target_bytes },
+                       SimilarityThreshold=SIMILARITY_THRESHOLD,
+        )
 
-    print ('Goodbye', name)
+        logFile = open('compareFace.txt', 'w')
+        pp = pprint.PrettyPrinter(indent=4, stream=logFile)
+        pp.pprint(response) 
+        
+        response = client.detect_faces(
+                       Image={ 'Bytes': target_bytes },
+                       Attributes=["ALL"],
+        )
+        
+        logFile = open('detectFace.txt', 'w')
+        pp = pprint.PrettyPrinter(indent=4, stream=logFile)
+        pp.pprint(response) 
+        
+    print("FIN!")
 
-
-# This is the standard boilerplate that calls the main() function.
-if __name__ == '__main__':
-    main()
+main()
+    
